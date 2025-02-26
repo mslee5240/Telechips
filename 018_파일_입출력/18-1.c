5239f527723be55c035764981cdfb778adc751fe
mslee5240 <ms.lee5240@gmail.com>
2025-02-26 09:30:51 +0900

    02/26

--- 018_파일_입출력/18-1.c
+++ 018_파일_입출력/18-1.c
@@ -1,20 +1,10 @@
-// #define _CRT_SECURE_NO_WARNINGS
-// #include <stdio.h>
-
-
-//int main(void) {
-//    app();
-//
-//    return 0;
-//}
-
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int main(void) {
     FILE *fp;   // 파일 포인터
 
-    fp = fopen("C:\\Users\\eec04\\source\\repos\\Telechips\\Telechips\\연습장\\a.txt", "r");   // a.txt 파일을 읽기 전용으로 개방
+    fp = fopen("a.txt", "r");   // a.txt 파일을 읽기 전용으로 개방
     if (fp == NULL) {
         printf("파일이 열리지 않았습니다.\n");
         return 1;
---
+++ playground/main.c
@@ -0,0 +1,26 @@
+// #define _CRT_SECURE_NO_WARNINGS
+// #include <stdio.h>
+
+
+//int main(void) {
+//    app();
+//
+//    return 0;
+//}
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+    FILE* fp;   // 파일 포인터
+
+    fp = fopen("C:\\Users\\eec04\\source\\repos\\Telechips\\Telechips\\playground\\a.txt", "r");   // a.txt 파일을 읽기 전용으로 개방
+    if (fp == NULL) {
+        printf("파일이 열리지 않았습니다.\n");
+        return 1;
+    }
+    printf("파일이 열렸습니다.\n");
+    fclose(fp); // 파일 닫기
+
+    return 0;
+}
