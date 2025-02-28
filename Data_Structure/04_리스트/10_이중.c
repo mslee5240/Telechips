2150bd4b4c797e22207df2e502a1899aac029eb3
mslee5240 <ms.lee5240@gmail.com>
2025-02-28 13:51:24 +0900

    [02/28][Data Structure - Double_Linked_List]

---
+++ Data_Structure/04_리스트/10_이중.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "double_linked_list.h"
+
+typedef DListNode Book;
+
+// 도서 추가 함수
+void add_book(DListNode* head, )
+
+int main(void) {
+    // 이중 연결 리스트 헤더 생성
+    Book* head = (Book*)malloc(sizeof(Book));
+    init(head);
+
+
+    return 0;
+}
