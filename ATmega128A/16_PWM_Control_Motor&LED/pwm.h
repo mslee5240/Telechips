d9a0b34e57794ceaf8ff6c3e442a74cd3343bd47
mslee5240 <ms.lee5240@gmail.com>
2025-03-13 23:48:05 +0900

    [03/13][ATmega128A - PWM_Control_Motor&Led]

---
+++ ATmega128A/16_PWM_Control_Motor&LED/pwm.h
@@ -0,0 +1,12 @@
+﻿#define F_CPU   16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+#include "button.h"
+
+
+void init_timer3(void);
+void init_L298N(void);
+void pwm_fan_control_main(void);
+void L298N_pwm_fan_control_main(void);
