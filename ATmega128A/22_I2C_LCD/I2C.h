12b87c253c0a6459f49b4e8b259504c131801319
mslee5240 <ms.lee5240@gmail.com>
2025-03-19 21:32:32 +0900

    [03/19][ATmega128A - I2C_LCD]

---
+++ ATmega128A/22_I2C_LCD/I2C.h
@@ -0,0 +1,7 @@
+﻿void I2C_init(unsigned int baud);
+void I2C_start(void);
+void I2C_transmit(uint8_t data);
+void I2C_write_byte(uint8_t address, uint8_t data);
+void I2C_stop(void);
+uint8_t I2C_receive_ACK(void);
+uint8_t I2C_receive_NACK(void);
